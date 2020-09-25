225ee7d5c18d759f2988d98cbe386d881080d8cb
Thorsten Hans <thorsten.hans@gmail.com>
2020-09-25 16:34:28 +0200

    cert-manager bump
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- kubernetes/03-deploy-certmanager.sh
+++ kubernetes/03-deploy-certmanager.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.yaml
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.1/cert-manager.crds.yaml
 
  helm repo add jetstack https://charts.jetstack.io
  helm repo update
 
- helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.16.1
+ helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.0.1 
